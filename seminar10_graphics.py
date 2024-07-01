647c9cd191ab16fe82c50c9ac399246162adb266
Евгений Иванов <evgen.iv.csm94@gmail.com>
2024-07-01 18:43:29 +0300

    Промежуточная аттестация

---
+++ seminar10_graphics.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+one_hot = pd.DataFrame()
+
+for category in data['whoAmI'].unique():
+    one_hot[category] = (data['whoAmI'] == category).astype(int)
+
+result = pd.concat([data, one_hot], axis=1)
+print(result)
